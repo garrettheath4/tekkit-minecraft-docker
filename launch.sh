6570dee33e64328720771faf9538a74e89343eca
Garrett Koller <gkoller@catalist.us>
2020-09-09 10:10:31 -0400

    Minor: Fixed usage of -i argument of sed in launch.sh script

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "$SERVER_OP" > /minecraft/ops.txt
-sed -i '.orig' -e "s/^motd=.*/motd=${SERVER_MOTD:-A Tekkit Server}/" -e "s/^pvp=.*/pvp=${SERVER_PVP:-true}/" /minecraft/server.properties
+sed -i.orig -e "s/^motd=.*/motd=${SERVER_MOTD:-A Tekkit Server}/" -e "s/^pvp=.*/pvp=${SERVER_PVP:-true}/" /minecraft/server.properties
 
 touch /minecraft/server.log
 tmux new-session -d -s tekkit "java $JAVA_ARGS -jar Tekkit.jar nogui"
